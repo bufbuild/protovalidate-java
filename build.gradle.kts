aefec2ea6b8d19c19a12a847737665889003a5f0
Steve Ayers <sayers@buf.build>
2025-04-30 11:18:58 -0400

    Adds some additional formatting tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -208,7 +208,6 @@ allprojects {
     tasks.withType<Test>().configureEach {
         useJUnitPlatform()
         this.testLogging {
-            this.showStandardStreams = true
             events("failed")
             exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
             showExceptions = true
