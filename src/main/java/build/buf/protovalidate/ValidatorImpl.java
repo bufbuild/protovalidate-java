99ddf43dd362903e319c5e0b584c129f7aac8137
Steve Ayers <sayers@buf.build>
2025-05-09 11:50:07 -0400

    More tests

--- src/main/java/build/buf/protovalidate/ValidatorImpl.java
+++ src/main/java/build/buf/protovalidate/ValidatorImpl.java
@@ -15,7 +15,6 @@
 package build.buf.protovalidate;
 
 import build.buf.protovalidate.exceptions.CompilationException;
-import build.buf.protovalidate.exceptions.ExecutionException;
 import build.buf.protovalidate.exceptions.ValidationException;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Message;
@@ -47,19 +46,7 @@ class ValidatorImpl implements Validator {
     this.failFast = config.isFailFast();
   }
 
-  /**
-   * Checks that message satisfies its rules. Rules are defined within the Protobuf file as options
-   * from the buf.validate package. A {@link ValidationResult} is returned which contains a list of
-   * violations. If the list is empty, the message is valid. If the list is non-empty, the message
-   * is invalid. An exception is thrown if the message cannot be validated because the evaluation
-   * logic for the message cannot be built ({@link CompilationException}), or there is a type error
-   * when attempting to evaluate a CEL expression associated with the message ({@link
-   * ExecutionException}).
-   *
-   * @param msg the {@link Message} to be validated.
-   * @return the {@link ValidationResult} from the evaluation.
-   * @throws ValidationException if there are any compilation or validation execution errors.
-   */
+  /** {@inheritDoc} */
   @Override
   public ValidationResult validate(Message msg) throws ValidationException {
     if (msg == null) {
--- src/test/java/build/buf/protovalidate/ValidatorConstructionTest.java
+++ src/test/java/build/buf/protovalidate/ValidatorConstructionTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.fail;
 import build.buf.protovalidate.exceptions.ValidationException;
 import com.example.imports.validationtest.ExampleFieldRules;
 import com.example.imports.validationtest.FieldExpressionMapInt32;
+import com.example.imports.validationtest.FieldExpressionMultiple;
 import com.google.protobuf.Descriptors.Descriptor;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,6 +30,8 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class ValidatorConstructionTest {
+
+  // Tests validation works as planned with default instance.
   @Test
   public void testDefaultInstance() {
     Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
@@ -47,8 +50,9 @@ public class ValidatorConstructionTest {
     }
   }
 
+  // Tests validation works as planned with normal builder and config
   @Test
-  public void testLazyBuilderWithConfig() {
+  public void testNormalBuilderWithConfig() {
     Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
     testMap.put(42, 42);
     FieldExpressionMapInt32 msg = FieldExpressionMapInt32.newBuilder().putAllVal(testMap).build();
@@ -66,6 +70,8 @@ public class ValidatorConstructionTest {
     }
   }
 
+  // Tests that if the correct seed descriptors are provided and lazy is disabled,
+  // validation works as planned.
   @Test
   public void testSeedDescriptorsLazyDisabled() {
     Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
@@ -90,6 +96,39 @@ public class ValidatorConstructionTest {
     }
   }
 
+  // Tests that the seed descriptor list is immutable inside the validator and that if
+  // a descriptor is removed after the validator is created, validation still works as planned.
+  @Test
+  public void testSeedDescriptorsImmutable() {
+    Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
+    testMap.put(42, 42);
+    FieldExpressionMapInt32 msg = FieldExpressionMapInt32.newBuilder().putAllVal(testMap).build();
+
+    List<Descriptor> seedDescriptors = new ArrayList<Descriptor>();
+    FieldExpressionMapInt32 reg = FieldExpressionMapInt32.newBuilder().build();
+    seedDescriptors.add(reg.getDescriptorForType());
+
+    Config cfg = Config.newBuilder().setFailFast(true).build();
+    try {
+      Validator validator =
+          ValidatorFactory.newBuilder(seedDescriptors, true).withConfig(cfg).build();
+
+      // Remove descriptor from list after the validator is created to verify validation still works
+      seedDescriptors.clear();
+
+      ValidationResult result = validator.validate(msg);
+      assertThat(result.isSuccess()).isFalse();
+      assertThat(result.getViolations().size()).isEqualTo(1);
+      assertThat(result.getViolations().get(0).toProto().getMessage())
+          .isEqualTo("all map values must equal 1");
+    } catch (ValidationException e) {
+      fail("unexpected exception thrown", e);
+    }
+  }
+
+  // Tests that if a message is attempted to be validated and it wasn't in the initial
+  // list of seed descriptors AND lazy is disabled, that a message is returned that
+  // no evaluator is available.
   @Test
   public void testSeedDescriptorsWithWrongDescriptorAndLazyDisabled() {
     Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
@@ -114,6 +153,8 @@ public class ValidatorConstructionTest {
     }
   }
 
+  // Tests that an IllegalStateException is thrown if an empty descriptor list is given
+  // and lazy is disabled.
   @Test
   public void testSeedDescriptorsWithInvalidState() {
     List<Descriptor> seedDescriptors = new ArrayList<Descriptor>();
@@ -124,6 +165,8 @@ public class ValidatorConstructionTest {
             });
   }
 
+  // Tests that when an empty list of seed descriptors is provided and lazy is enabled
+  // that the missing message descriptor is successfully built and validation works as planned.
   @Test
   public void testEmptySeedDescriptorsLazyEnabled() {
     Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
@@ -145,24 +188,48 @@ public class ValidatorConstructionTest {
     }
   }
 
-  // @Test
-  // public void testDisableLazy() {
-  //   Map<Integer, Integer> testMap = new HashMap<Integer, Integer>();
-  //   testMap.put(42, 42);
-  //   FieldExpressionMapInt32 msg =
-  // FieldExpressionMapInt32.newBuilder().putAllVal(testMap).build();
+  // Tests that the config is applied when building a validator.
+  @Test
+  public void testConfigApplied() {
+    // Value must be at most 5 characters and must be lowercase alpha chars or numbers.
+    FieldExpressionMultiple msg = FieldExpressionMultiple.newBuilder().setVal("INVALID").build();
 
-  //   Config cfg = Config.newBuilder().setDisableLazy(true).build();
-  //   Validator validator = new Validator(cfg);
-  //   try {
-  //       System.err.println("goosh");
-  //       validator.loadMessages(FieldExpressionMapInt32.newBuilder().build());
-  //     ValidationResult result = validator.validate(msg);
-  //       assertThat(result.isSuccess()).isFalse();
-  //       assertThat(result.getViolations().size()).isEqualTo(1);
-  //       System.err.println(result);
-  //   } catch (Exception e) {
-  //       System.err.println("AAAAAAAAAAAAAAAAAAAAAAAAAa");
-  //   }
-  // }
+    // Set fail fast to true, so we exit after the first validation failure.
+    Config cfg = Config.newBuilder().setFailFast(true).build();
+    try {
+      Validator validator = ValidatorFactory.newBuilder().withConfig(cfg).build();
+      ValidationResult result = validator.validate(msg);
+      assertThat(result.isSuccess()).isFalse();
+      assertThat(result.getViolations().size()).isEqualTo(1);
+      assertThat(result.getViolations().get(0).toProto().getMessage())
+          .isEqualTo("value length must be at most 5 characters");
+    } catch (ValidationException e) {
+      fail("unexpected exception thrown", e);
+    }
+  }
+
+  // Tests that the config is applied when building a validator with seed descriptors.
+  @Test
+  public void testConfigAppliedWithSeedDescriptors() {
+    // Value must be at most 5 characters and must be lowercase alpha chars or numbers.
+    FieldExpressionMultiple msg = FieldExpressionMultiple.newBuilder().setVal("INVALID").build();
+
+    FieldExpressionMultiple desc = FieldExpressionMultiple.newBuilder().build();
+    List<Descriptor> seedDescriptors = new ArrayList<Descriptor>();
+    seedDescriptors.add(desc.getDescriptorForType());
+
+    // Set fail fast to true, so we exit after the first validation failure.
+    Config cfg = Config.newBuilder().setFailFast(true).build();
+    try {
+      Validator validator =
+          ValidatorFactory.newBuilder(seedDescriptors, false).withConfig(cfg).build();
+      ValidationResult result = validator.validate(msg);
+      assertThat(result.isSuccess()).isFalse();
+      assertThat(result.getViolations().size()).isEqualTo(1);
+      assertThat(result.getViolations().get(0).toProto().getMessage())
+          .isEqualTo("value length must be at most 5 characters");
+    } catch (ValidationException e) {
+      fail("unexpected exception thrown", e);
+    }
+  }
 }
