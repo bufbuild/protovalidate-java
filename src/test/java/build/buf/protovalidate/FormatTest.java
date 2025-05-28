5f4f04082e8aed0ca962d5dd05027079525ee09f
Steve Ayers <sayers@buf.build>
2025-05-28 10:16:02 -0400

    Tests

--- src/test/java/build/buf/protovalidate/FormatTest.java
+++ src/test/java/build/buf/protovalidate/FormatTest.java
@@ -71,11 +71,20 @@ class FormatTest {
 
   private static List<String> SKIPPED_TESTS =
       Arrays.asList(
+          // Success Tests
+          // found no matching overload for 'format' applied to 'string.(list(type(string)))'
           "type() support for string",
+          // found no matching overload for 'format' applied to 'string.(list(map(string, dyn)))'
           "map support for string",
+          // found no matching overload for 'format' applied to 'string.(list(map(dyn, dyn)))'
           "map support (all key types)",
+          // found no matching overload for 'format' applied to 'string.(list(map(dyn, dyn)))'
           "dyntype support for maps",
+          // Error Tests
+          // found no matching overload for 'format' applied to
+          // 'string.(list(cel.expr.conformance.proto3.TestAllTypes))'
           "object not allowed",
+          // found no matching overload for 'format' applied to 'string.(list(map(int, dyn)))'
           "object inside map");
 
   @BeforeAll
